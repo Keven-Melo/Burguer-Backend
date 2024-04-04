6a260ae9b977b96882220d773ee8955fa1eb4f1a
Keven <kevenmelodn@gmail.com>
2024-04-04 16:40:10 -0300

    Alteracoes Category

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -18,11 +18,21 @@ class CategoryController {
 
       const {name} = request.body
 
-      const category = await Category.create({
+      const categoryExists = await Category.findOne({
+        where: {
+          name,
+        }
+      })
+
+      if(categoryExists){
+        return response.status(400).json({ error: 'Category already exists'})
+      }
+
+      const {id} = await Category.create({
         name
       });
 
-      return response.json({category})
+      return response.json({name, id})
   }
   async index(request, response){
     const category = await Category.findAll()
